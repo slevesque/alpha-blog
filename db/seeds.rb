cb681f0e24181a37fd6ea137bd09248c1c7ad0a2
Sylvain Lévesque <sylvainl@demarque.ca>
2018-05-28 15:04:56 -0400

    Create articles related things.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = Article.create([
+  {title: 'My first article', description: 'My first article description'},
+  {title: 'My second article', description: 'My second article description'}
+])
