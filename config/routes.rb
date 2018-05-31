3ebeecaf1665c0dc3cc5acb04f073d2bdb3526da
Sylvain Lévesque <sylvainl@demarque.ca>
2018-05-31 15:21:51 -0400

    Add minimal stuff for login and session.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'pages/about'
   resources :articles
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
