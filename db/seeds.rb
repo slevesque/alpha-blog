5c9a5ec04a63c2035d61bd048b61c5a6a560b8b8
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-01 12:05:57 -0400

    Another seeds try

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users - User.create([
-  {username: 'slevesque', email: 'slevesque@gezere.com', password: '1234'}
+  {username: 'slevesque', email: 'slevesque@gezere.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm'}
 ])
 # {username: 'slevesque', email: 'slevesque@gezere.com', password: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm'}
 
