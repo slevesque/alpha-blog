59a541643110b86dc7dfcddf170071bbc63561d8
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-01 16:26:38 -0400

    Redirect the new signed user to his homepage.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, except: [:index, :show, :new]
+  before_action :require_user, except: [:index, :show, :new, :create]
   before_action :require_same_user, only: [:edit, :update] 
 
   def index
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -48,7 +49,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if !logged_in? && current_user != @user
+    if current_user != @user
       flash[:danger] = 'You can only edit your  own account'
       redirect_to root_path
     end
