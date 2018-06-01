13c46e8a0e9d15819a5a2bd83299252ab20a1c02
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-01 11:51:35 -0400

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users - User.create([
+  {username: 'slevesque', email: 'slevesque@gezere.com', password = '1234'}
+])
+
 articles = Article.create([
-  {title: 'My first article', description: 'My first article description'},
-  {title: 'My second article', description: 'My second article description'}
+  {title: 'My first article', description: 'My first article description', user_id: 1},
+  {title: 'My second article', description: 'My second article description', user_id: 1}
 ])
