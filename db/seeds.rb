70365930e331539814514d182f12d6a08f38ffeb
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-01 12:07:43 -0400

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users - User.create([
+users = User.create([
   {username: 'slevesque', email: 'slevesque@gezere.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm'}
 ])
 # {username: 'slevesque', email: 'slevesque@gezere.com', password: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm'}
