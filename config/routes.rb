9926206c5b086ecb4e833d91a0a2236265893434
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-04 14:03:51 -0400

    Add my firsts unit test.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
