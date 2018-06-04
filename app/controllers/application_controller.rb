3808d76a85a0b71e6a02b6c09fdd99cb96b6ab1f
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-04 10:42:36 -0400

    Regive access to admin user to modify article of other user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
+    # reset_session
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
