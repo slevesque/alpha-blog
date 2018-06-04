25570cf26ca82e1cab426f6b50cc344703bdf706
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-04 09:38:16 -0400

    Switch gezere.com => demarque.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create([
-  {id: 1, username: 'admin', email: 'admin@gezere.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm', admin: true},
-  {id: 2, username: 'user', email: 'user@gezere.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm', admin: false}
+  {id: 1, username: 'admin', email: 'admin@demarque.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm', admin: true},
+  {id: 2, username: 'user', email: 'user@demarque.com', password_digest: '$2a$10$6tjgTwdPG7DySw8YM5OJXu3964jA4pYe2xKYzbKKJYOKrgOK.skYm', admin: false}
 ])
 
 articles = Article.create([
