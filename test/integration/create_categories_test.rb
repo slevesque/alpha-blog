58e51cf5bafc3d8537aee8cb27d62167009c9df4
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-05 10:04:23 -0400

    Add new category with integration tests.

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,7 +5,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post categories_path, params: {name: 'sports'}
+      post categories_path, params: {category: {name: 'sports'}}
       follow_redirect!
     end
     assert_template 'categories/index'
